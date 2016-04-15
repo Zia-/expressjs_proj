61f7a508b93313d119babcfb94fd4340977c49d1
Mohammed Zia <mohammed.zia33@gmail.com>
2016-04-15 17:20:05 +0300

    Mongoldb connected
    Connected to mongoldb and url is working fine

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
+//Include mongoose to our project
 var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
@@ -28,7 +31,54 @@ app.use('/', routes);
 //app.use('/users', users);
 
 //Connect to mongoose
-mongoose.connect('mongodb://160.75.81.194/test')
+mongoose.connect('mongodb://160.75.81.194:27017/test', function(error){
+  if (error){
+    console.log(error);
+  };
+  console.log("Mongo Connection Successful :)");
+});
+
+var personSchema = {
+  node: {id: String, lat: String,
+        lon: String, version: String, timestamp: String,
+        changeset: String, uid: String, user: String}
+};
+
+var Person = mongoose.model('Person', personSchema, 'nodes_collection');
+app.get('/peop', function(req, res){
+  Person.find({'node.version': '5'}, function(err, doc){
+    res.send(doc);
+  })
+})
+
+// var schemamongo = mongoose.Schema;
+// var dataSchema = new schemamongo({
+//   _id: schemamongo.Types.ObjectId,
+//   node: {id: String, lat: String,
+//         lon: String, version: String, timestamp: String,
+//         changeset: String, uid: String, user: String}
+// });
+//
+// var dataget = mongoose.model('nodes_collection', dataSchema, 'files');
+// app.get('/nodes_url', function(req, res){
+//   dataget.find({'node.version': '5'}, function(err, nodes){
+//     res.send(nodes);
+//   });
+//   //res.send("hi");
+//
+//   // dataget.find({}, function(err, node){
+//   //   if(err){
+//   //           console.log('error..');
+//   //           return handleError(err);
+//   //       }
+//   //       else {
+//   //           console.log('returning data..');
+//   //           // res.render( "parkings", { version: 1 });
+//   //           res.send(node);
+//   //       }
+//   // });
+// });
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
